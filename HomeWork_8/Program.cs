c3e7f858866bdf530cc79c79eddbd013107dbc66
Marina Barinova <marina.alive888@yandex.ru>
2023-02-06 21:31:21 +0300

    Задача 8 с решением

---
+++ HomeWork_8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 8. Напишите программу, которая на вход принимает число N, а на выходе показывает все четные числа от 1 до N.
+
+Console.Write("input your number N: ");
+int num_N = Convert.ToInt32(Console.ReadLine());
+int current_num_N = 1;
+while(current_num_N <= num_N)
+{
+    Console.Write(current_num_N +" ");
+    current_num_N++;
+}
