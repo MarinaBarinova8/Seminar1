1290d80ab640084f88e3d57ffc75ea50e5266d55
Marina Barinova <marina.alive888@yandex.ru>
2023-02-06 20:59:26 +0300

    Задача 6 с рещением

---
+++ HomeWork_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6. Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на 2 без остатка)
+
+Console.Write("input your  number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+  Console.WriteLine("Your number is even");  
+}
+else
+{
+    Console.WriteLine("Your number is odd"); 
+}
