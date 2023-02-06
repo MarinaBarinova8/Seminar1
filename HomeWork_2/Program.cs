5984088cbe20ef6fc5aec74931d51d1d2d63dd2b
Marina Barinova <marina.alive888@yandex.ru>
2023-02-06 20:13:49 +0300

    Задача-2 с рещением

--- HomeWork_2/Program.cs
+++ HomeWork_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2. Напишите программу, котрая на вход принимает два числа и выдает какое число большее, а какое меньшее
+
+Console.Write("Input yoyr first number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input yoyr second number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (number_1 > number_2)
+{
+    Console.WriteLine($"max={number_1}, min={number_2}");
+}
+else
+{
+    Console.WriteLine($"max={number_2}, min={number_1}");
+}
+
